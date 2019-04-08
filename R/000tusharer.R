e5374fc6f9694759d090d4c4ebf398dba7bf0e1f
Jun <imlijunda@gmail.com>
2019-04-08 14:11:10 +0930

    Added package rd

--- R/000tusharer.R
+++ R/000tusharer.R
@@ -1,3 +1,12 @@
 #' @import data.table
 #' @import httr
 NULL
+
+#' @details
+#' TushareR is a wrapper for Tushare Pro API \url{https://tushare.pro}. Please
+#' refer to the online document for details of the APIs.
+#'
+#' Prior to using TushareR, you need to set your API token by calling
+#' SetToken("YOUR_API_TOKEN").
+#'
+"_PACKAGE"
